ba7174c9be2f827c4a8bed9bfe6168b25d7e059a
GreyRing <greyring@163.com>
2017-11-21 20:57:55 +0800

    11.221modify vga_controller, use bus_interface control module untested

--- top.v
+++ top.v
@@ -31,9 +31,9 @@ module top(
 	 input [15:0] switch,
     input RSTN,
     
-   output led_clk,
-   output led_do,
-   output led_pen,
+   //output led_clk,
+   //output led_do,
+   //output led_pen,
    output seg_clk,
    output seg_do,
    output seg_pen,
@@ -81,6 +81,67 @@ input_switch_btn Input_switch_btn(
 wire Clk_CPU;
 assign Clk_CPU = SW_OK[2]? Div[24]:Div[0];
 
+wire [31:0]addr_bus;
+wire [31:0]data_bus;
+wire [2:0]ctrl_bus;
+wire en_SRAM, en_BIOS, en_vga_reg, en_cursor_reg, en_textRAM, 
+		en_graphRAM, en_DRAM, en_SEG, en_others;
+addr_decoder Addr_decoder(
+    .addr(addr_bus), 
+    .en_SRAM(en_SRAM), .en_BIOS(en_BIOS), .en_vga_reg(en_vga_reg),     
+	 .en_cursor_reg(en_cursor_reg), .en_textRAM(en_textRAM),     
+	 .en_graphRAM(en_graphRAM), .en_DRAM(en_DRAM), .en_SEG(en_SEG), 
+    .en_others(en_others)
+    );
+wire [31:0]SRAM_addr, SRAM_wdata, SRAM_rdata;
+wire SRAM_r, SRAM_w;
+bus_interface SRAM(
+    .enable(en_SRAM), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(SRAM_addr), .wdata(SRAM_wdata), .rdata(SRAM_rdata), 
+	 .r_(SRAM_r), .w_(SRAM_w), .ready_(1'b1)
+    );
+wire [31:0]vga_reg_wdata, vga_reg_rdata;
+wire vga_reg_r, vga_reg_w;
+bus_interface VGA_REG(
+    .enable(en_vga_reg), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(), .wdata(vga_reg_wdata), .rdata(vga_reg_rdata), 
+	 .r_(vga_reg_r), .w_(vga_reg_w), .ready_(1'b1)
+    );
+wire [31:0]vga_cursor_wdata, vga_cursor_rdata;
+wire vga_cursor_r, vga_cursor_w;
+bus_interface VGA_CURSOR(
+    .enable(en_cursor_reg), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(), .wdata(vga_cursor_wdata), .rdata(vga_cursor_rdata), 
+	 .r_(vga_cursor_r), .w_(vga_cursor_w), .ready_(1'b1)
+    );
+wire [31:0]vga_text_addr, vga_text_wdata, vga_text_rdata;
+wire vga_text_r, vga_text_w;
+bus_interface VGA_TEXT(
+    .enable(en_textRAM), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(vga_text_addr), .wdata(vga_text_wdata), .rdata(vga_text_rdata), 
+	 .r_(vga_text_r), .w_(vga_text_w), .ready_(1'b1)
+    );
+wire [31:0]vga_graph_addr, vga_graph_wdata, vga_graph_rdata;
+wire vga_graph_r, vga_graph_w;
+bus_interface VGA_GRAPH(
+    .enable(en_graphRAM), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(vga_graph_addr), .wdata(vga_graph_wdata), .rdata(vga_graph_rdata), 
+	 .r_(vga_graph_r), .w_(vga_graph_w), .ready_(1'b1)
+    );
+wire SEG_r, SEG_w;
+wire [31:0]SEG_wdata;
+bus_interface SEG(
+    .enable(en_SEG), 
+    .addr(addr_bus), .data(data_bus), .r(ctrl_bus[0]), .w(ctrl_bus[1]), .ready(ctrl_bus[2]), 
+    .addr_(), .wdata(SEG_wdata), .rdata(32'h0), 
+	 .r_(SEG_r), .w_(SEG_w), .ready_(1'b1)
+    );
+	 
 wire [31:0]Disp_num;
 wire [7:0]point_out;
 wire [7:0]LE_out;
@@ -98,28 +159,25 @@ wire [7:0]LE_out;
     .SEGEN(seg_pen), 
     .segclrn()
     );
-	 
-wire [31:0]Peripheral_in;
-wire GPIOe0000000_we;
+
 wire [31:0]PC;
 wire [31:0]inst;
-wire [31:0]Counter_out;
-wire [31:0]Addr_out;
-wire [31:0]Data_out;
+//wire [31:0]Addr_out;
+//wire [31:0]Data_out;
 wire [31:0]Data_in;
 wire [31:0]cause_data;
 wire [31:0]status_data;
 	Multi_8CH32  multi_8ch32(
 		.clk(~Clk_CPU), 
-		.Data0(Peripheral_in), 
+		.Data0(SEG_wdata), 
 		.data1(cause_data[31:0]), 
 		.data2(inst[31:0]), 
 		.data3(status_data[31:0]), 
-		.data4(Addr_out[31:0]), 
-		.data5(Data_out[31:0]), 
+		.data4(addr_bus[31:0]), 
+		.data5(data_bus[31:0]), 
 		.data6(Data_in[31:0]), 
 		.data7(PC[31:0]), 
-		.EN(GPIOe0000000_we), 
+		.EN(SEG_w), 
 		.LES(64'b0), 
 		.point_in({Div[31:0], Div[31:0]}), 
 		.rst(rst), 
@@ -129,70 +187,29 @@ wire [31:0]status_data;
 		.point_out(point_out)
 		);
 
-wire counter0_OUT,counter1_OUT,counter2_OUT;
-wire [15:0]LED_out;
-wire mem_w;
-wire mem_rd;
-wire [31:0]ram_data_in;
-wire counter_we;
-wire data_ram_we;
-wire GPIOf0000000_we;
-wire [9:0]ram_addr;
-wire [31:0]douta;
-wire vga_we_t, vga_we_g, vga_we_c, vga_we_r;
-wire vga_rd_t, vga_rd_g, vga_rd_c, vga_rd_r;
-wire [31:0]vga_addr;
-wire [31:0]vga_data;
-wire [31:0]vga_data_out;
-MIO  mio_bus(
-	  .addr_bus(Addr_out[31:0]), 
-	  .counter_out(Counter_out), 
-	  .counter0_out(counter0_OUT), 
-	  .counter1_out(counter1_OUT), 
-	  .counter2_out(counter2_OUT), 
-	  .Cpu_data2bus(Data_out[31:0]), 
-	  .led_out(LED_out), 
-	  .mem_w(mem_w), 
-	  .mem_rd(mem_rd),
-	  .ram_data_out(douta), 
-	  .SW(SW_OK), 
-	  .counter_we(counter_we), 
-	  .Cpu_data4bus(Data_in), 
-	  .data_ram_we(data_ram_we), 
-	  .GPIOe0000000_we(GPIOe0000000_we), 
-	  .GPIOf0000000_we(GPIOf0000000_we), 
-	  .Peripheral_in(Peripheral_in), 
-	  .ram_addr(ram_addr), 
-	  .ram_data_in(ram_data_in),
-	  .vga_data_out(vga_data_out),
-	  .vga_we_t(vga_we_t),
-	  .vga_we_g(vga_we_g),
-	  .vga_we_c(vga_we_c),
-	  .vga_we_r(vga_we_r),
-	  .vga_rd_t(vga_rd_t),
-	  .vga_rd_g(vga_rd_g),
-	  .vga_rd_c(vga_rd_c),
-	  .vga_rd_r(vga_rd_r),
-	  .vga_addr(vga_addr),
-	  .vga_data(vga_data)
-	  );
-
 vga_controller Vga_controller(
     .clk(clk_100mhz), 
     .rst(rst), 
     .clk_vga(Div[1]), 
     .clk_cursor(Div[25]), 
-    .we_text(vga_we_t), 
-    .we_graph(vga_we_g), 
-    .we_cursor(vga_we_c), 
-    .we_reg(vga_we_r), 
-    .rd_text(vga_rd_t), 
-    .rd_graph(vga_rd_g), 
-    .rd_cursor(vga_rd_c), 
-    .rd_reg(vga_rd_r), 
-    .addr_in(vga_addr), 
-    .data_in(vga_data), 
-    .data_out(vga_data_out), 
+    .we_text(vga_text_w), 
+    .we_graph(vga_graph_w), 
+    .we_cursor(vga_cursor_w), 
+    .we_reg(vga_reg_w), 
+	 .rd_text(vga_text_r), 
+    .rd_graph(vga_graph_r), 
+    .rd_cursor(vga_cursor_r), 
+    .rd_reg(vga_reg_r), 
+    .text_addr(vga_text_addr), 
+    .text_wdata(vga_text_wdata), 
+    .text_rdata(vga_text_rdata), 
+    .graph_addr(vga_graph_addr), 
+    .graph_wdata(vga_graph_wdata), 
+    .graph_rdata(vga_graph_rdata), 
+    .cursor_wdata(vga_cursor_wdata), 
+    .cursor_rdata(vga_cursor_rdata), 
+    .reg_wdata(vga_reg_wdata), 
+    .reg_rdata(vga_reg_rdata), 
     .r(vga_red), 
     .g(vga_green), 
     .b(vga_blue), 
@@ -200,7 +217,7 @@ vga_controller Vga_controller(
     .vsync(vga_v_sync), 
     .busy()
     );
-	  
+/*
 wire [1:0]counter_set;
 GPIO gpio(
     .clk(~Clk_CPU), 
@@ -233,7 +250,8 @@ GPIO gpio(
 	  .counter1_OUT(counter1_OUT), 
 	  .counter2_OUT(counter2_OUT)
 	  );
-
+*/
+wire [31:0]CPU_wdata;
    PCPU_v PCPU(
 		.clk(Clk_CPU & ~SW_OK[15]), 
 		`ifdef DEBUG
@@ -242,16 +260,17 @@ GPIO gpio(
 		.rst(rst),
 		`endif
 		.int_(5'b0),
-		.mem_we(mem_w), 
-		.mem_rd(mem_rd),
-		.mem_addr(Addr_out), 
-		.mem_data(Data_out), 
+		.mem_we(ctrl_bus[1]), 
+		.mem_rd(ctrl_bus[0]),
+		.mem_addr(addr_bus), 
+		.mem_data(CPU_wdata), 
 		.inst_addr(PC), 
 		.inst_data(inst), 
 		.cause_data(cause_data),
 		.status_data(status_data),
-		.mem_data_in(Data_in)
+		.mem_data_in(data_bus)
    );
+assign data_bus = ctrl_bus[1] ? CPU_wdata : 32'hz;
 /*
 bus Bus(
     .master_addr(master_addr), 
@@ -283,10 +302,10 @@ bus Bus(
 	
 	Data_RAM Data_RAM_(
 	  .clka(~Clk_CPU), 
-	  .wea(data_ram_we), 
-	  .addra(ram_addr[9:0]), 
-	  .dina(ram_data_in), 
-	  .douta(douta) 
+	  .wea(SRAM_w), 
+	  .addra(SRAM_addr[11:2]), 
+	  .dina(SRAM_wdata), 
+	  .douta(SRAM_rdata) 
 	);
 	
 	Inst_ROM Inst_ROM_(
